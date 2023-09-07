7f1ff4ce18f306a9e493fd8f784013a218865651
user uaers <thatkrsn21@gmail.com>
2023-09-07 18:53:33 +0300

    задание 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа
+Console.WriteLine("Ввидите число");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+
+    if (N %2 == 0 )
+    {
+        Console.WriteLine(i);
+
+    }
+
+}
