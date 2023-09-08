178d5613078ec544a9eddf4ec515cd9c69dbd383
user uaers <thatkrsn21@gmail.com>
2023-09-08 14:05:41 +0300

    Добавили решение

--- task4/Program.cs
+++ task4/Program.cs
@@ -8,7 +8,7 @@ for (int i = 0; i < N; i+=2)
     if (i %2 == 0 )
     {
         Console.Write(i);
-
     }
+    
 
 }
---
+++ task7/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Ввидите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+int secondDigit = (number / 10) % 10;
+
+Console.WriteLine($" Показывает вторую цифру {secondDigit }");
