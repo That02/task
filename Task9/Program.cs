3d2164e6043f994facc3f9fc2e056b0a71e43db4
user uaers <thatkrsn21@gmail.com>
2023-09-08 19:46:08 +0300

    Добавили решения

---
+++ Task9/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер дня недели (1 - ПН, 2 - ВТ 3 - СР, 4 - ЧТ, 5-ПТ, 6 - СБ, 7 - ВС): ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        if (IsWeekend(dayOfWeek))
+        {
+            Console.WriteLine("Этот день является выходным.");
+        }
+        else
+        {
+            Console.WriteLine("Этот день не является выходным.");
+        }
+    }
+
+    static bool IsWeekend(int dayOfWeek)
+    {
+        
+        return dayOfWeek == 6 || dayOfWeek == 7;
+    }
+}
