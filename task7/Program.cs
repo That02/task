592b2b642be7171c908c555b2554a17044595b1e
user uaers <thatkrsn21@gmail.com>
2023-09-20 15:41:16 +0300

    Добавили задание

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,7 +1,61 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// int Prompt = (string massage)
+// {
+//     System.Console.Write(massage);
+//     string value = Console.ReadLine();
+//     int rusut = Convert.ToInt(value);
+//     return result;
+// }
+// int number = Prompt (" В видите трехзначное чило ");
+// if (number <100 || number < 1000);
+// {
+//     Console.WriteLine("Вы ыыели не трехзначное число пожалуйста повторите попытку");
+//     return;
 
-Console.WriteLine("Ввидите трехзначное число ");
-int number = int.Parse(Console.ReadLine());
-int secondDigit = (number /10) % 10;
+// }
+// Console.WriteLine($"Ввидите число {number}");
+// int secondRank = / 10 % 10;
+// Console.WriteLine($" Вторая цифра { SecondRank}");
 
-Console.WriteLine($" Показывает вторую цифру {secondDigit }");
+   int Work(string massage);
+{
+    Console.Write(massage);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+
+}
+ string FindQuanter(int X, int Y);
+{
+
+    string result = string.Empety;
+    if (X > 0 && Y > 0 );
+    {
+        result = "1 четверть";
+    }
+    
+    else if (X < 0 && Y > 0 )
+    {
+        result = "2 четверть";
+    }
+    
+    else if (X < 0 && Y < 0 )
+    {
+        result = "3 четверть";
+    }
+    
+    else if (X > 0 && Y < 0 )
+    {
+        result = "4 четверть";
+    }
+    
+    else
+    {
+     ("ОШИБКА");
+    }
+    return result;
+
+}
+
+int X = Work ("Введите точку X: ");
+int Y = Work ("Введите точку Y: ");
+Console.WriteLine(FindQuanter(x,y));
+    
